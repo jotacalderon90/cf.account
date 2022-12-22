22220c1433f2b925471071b825d9d6be3c5d8c18
jhonatan calderon <jcalderon.aprog@gmail.com>
2022-12-22 12:25:53 -0300

    avance

--- backend/01.account.views.js
+++ backend/01.account.views.js
@@ -8,38 +8,38 @@ module.exports = {
 	//@method(['get'])
 	//@roles(['root','admin','user'])
 	renderDocument: function(req,res){
-		res.render('document',{user: req.user});
+		res.render('views/account/document',{user: req.user});
 	},
 	
 	//@route('/form')
 	//@method(['get'])
 	renderForm: function(req,res){
-		res.render('form');
+		res.render('views/account/form');
 	},
 	
 	//@route('/login')
 	//@method(['get'])
 	renderLogin: function(req,res){
-		res.render('login', {google_auth: googleapis.getURL()});
+		res.render('views/account/login', {google_auth: googleapis.getURL()});
 	},
 	
 	//@route('/forget')
 	//@method(['get'])
 	renderForget: function(req,res){
-		res.render('forget');
+		res.render('views/account/forget');
 	},
 	
 	//@route('/recovery')
 	//@method(['get'])
 	renderRecovery: function(req,res){
-		res.render('recovery');
+		res.render('views/account/recovery');
 	},
 	
 	//@route('/admin/admin')
 	//@method(['get'])
 	//@roles(['root','admin'])
 	renderAdmin: function(req,res){
-		res.render('admin');
+		res.render('views/account/admin');
 	}
 	
 }
