25fbdc7b8c724b121c8bee193a26001fbd174758
jhonatan calderon <jcalderon.aprog@gmail.com>
2023-07-03 22:02:03 -0400

    avance

--- backend/05.account.api.google.send.js
+++ backend/05.account.api.google.send.js
@@ -1,14 +1,16 @@
 "use strict";
 
+const response = require('cl.jotacalderon.cf.framework/lib/response');
 const googleapis = require('./lib/googleapis');
 
 module.exports = {
 	
 	//@route('/api/google/send')
 	//@method(['post'])
+	//@roles(['root','googleapis'])
 	send: async function(req,res){
 		try{
-			res.send({data: await googleapis.sendMemo(req.user.tokens,req.body.raw)});
+			res.send({data: await googleapis.sendMemo(req.user.google.tokens,req.body.raw)});
 		}catch(e){
 			response.APIError(req,res,e);
 		}
