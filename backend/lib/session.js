3cfd63d1bc5585c921e43d51dc4532c63decb0f9
Jhonatan Calderon <jcalderon.aprog@gmail.com>
2026-05-09 11:13:13 -0400

    actualizacion framework multidomain

--- backend/lib/session.js
+++ backend/lib/session.js
@@ -10,27 +10,26 @@ const tracking = function (req, email, host) {
   req.session.ip = req.ip + '||' + req.headers['x-forwarded-for'];
 };
 
-const getCookie = function () {
+const getCookie = function (domain) {
   return {
     path: '/',
     httpOnly: true, // SIEMPRE
     maxAge: 1000 * 60 * 60, // SIEMPRE 1 hora
     secure: process.env.COOKIE_SECURE === '1', // POR PARAMETRO
     sameSite: process.env.COOKIE_SAMESITE || 'Strict', // SIEMPRE
-    //...(process.env.COOKIE_DOMAIN == 1 && { domain: domain }),
+    ...(process.env.COOKIE_DOMAIN == 1 && { domain: domain }),
   };
 };
 
-/*
 const getHost = function (host) {
   return '.' + host.split(':')[0].match(/([^.]+\.[^.]+)$/)[1];
-};*/
+};
 
 module.exports = {
   create: function (req, res, cookie, email, host) {
     logger.info('creando sesion');
 
-    res.cookie('Authorization', cookie, getCookie()); //getHost(req.headers.host)
+    res.cookie('Authorization', cookie, getCookie(getHost(req.headers.host)));
 
     if (cookie && email) {
       tracking(req, email, host);
@@ -47,6 +46,6 @@ module.exports = {
       }
     });
 
-    res.clearCookie('Authorization', getCookie()); //getHost(req.headers.host)
+    res.clearCookie('Authorization', getCookie(getHost(req.headers.host)));
   },
 };
