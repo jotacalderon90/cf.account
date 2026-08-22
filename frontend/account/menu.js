946b55a884ff83482e2a16f75b2396b96ad23254
Jhonatan Calderon <jcalderon.aprog@gmail.com>
2026-08-22 14:45:54 -0400

    menu avera

--- frontend/account/menu.js
+++ frontend/account/menu.js
@@ -4,7 +4,8 @@ const menu = function() {
     {name: 'archivospublicos',  label: 'Archivos Públicos', defaultPort: 2201, defaultRoles: []},
     {name: 'archivosprivados',  label: 'Archivos Privados', defaultPort: 2203, defaultRoles: ['archivosprivados']},
     {name: 'database',          label: 'Base de Datos',     defaultPort: 2204, defaultRoles: ['database']},
-    {name: 'mailing',           label: 'Mailing',           defaultPort: 2205, defaultRoles: ['mailing']}
+    {name: 'mailing',           label: 'Mailing',           defaultPort: 2205, defaultRoles: ['mailing']},
+    {name: 'avera',             label: 'Avera',             defaultPort: 2210, defaultRoles: ['avera']}
   ];
   
   this.collection = [];
