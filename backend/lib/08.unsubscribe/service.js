8f2a506d8df96e1213b336e0036539e863e9372f
Jhonatan Calderon <jcalderon.aprog@gmail.com>
2026-08-25 23:46:57 -0400

    hash en desubscribir varia por base64

--- backend/lib/08.unsubscribe/service.js
+++ backend/lib/08.unsubscribe/service.js
@@ -23,7 +23,7 @@ module.exports = {
         notification: false,
       };
 
-      return user.update(set, userByEmail.id);
+      return await user.update(set, userByEmail.id);
     } catch (error) {
       logger.error(error);
       throw new Error(constants.error.rest.unsubscribe + ' ' + constants.error.servicio);
