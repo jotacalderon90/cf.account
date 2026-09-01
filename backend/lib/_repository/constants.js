10f59a5c2653d87566c39ab5001f10e86fc4be8c
Jhonatan Calderon <jcalderon.aprog@gmail.com>
2026-08-31 22:44:06 -0400

    constante para tags

--- backend/lib/_repository/constants.js
+++ backend/lib/_repository/constants.js
@@ -6,7 +6,7 @@ const _constants = structuredClone(constants);
 
 _constants.error.rest.total = 'Error al obtener total';
 _constants.error.rest.collection = 'Error al buscar';
-
+_constants.error.rest.tags = 'Error al obtener tags';
 _constants.error.rest.create = 'Error al crear';
 _constants.error.rest.read = 'Error al leer';
 _constants.error.rest.update = 'Error al actualizar';
